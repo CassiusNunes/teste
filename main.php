dd79e769704ff316e71d8e9e926940ccb6c28a56
Cassius <cassius_nunes@yahoo.com>
2020-08-11 10:30:40 -0300

    Update main.php

--- main.php
+++ main.php
@@ -3,17 +3,14 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 use Moovin\Job\Backend;
-
+/*
 $exemplo = new Backend\Exemplo;
 
-echo $exemplo->exemplo();
+echo $exemplo->exemplo();*/
 
 $conta = new Backend\Conta('123456','1');
 
-echo $conta->Deposito('800');
-echo ' ';
-echo $conta->Saque('500');
-echo ' ';
-echo $conta->Tranferencia('456', '2', 200);
-echo ' ';
-echo "Saldo Atual: " . $conta->SaldoAtual();
+echo $conta->Deposito('800').PHP_EOL;
+echo $conta->Saque('500').PHP_EOL;
+echo $conta->Tranferencia('456', '2', 200).PHP_EOL;
+echo "Saldo Atual: " . $conta->SaldoAtual().PHP_EOL;
